4cb5f4ee48bce4d4a12c645d77ebab61d503349f
Zurina <bigler1205@gmail.com>
2022-07-12 13:31:41 +0200

    feat: first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/host_id", func(w http.ResponseWriter, r *http.Request) {
+		id := uuid.New()
+		fmt.Fprint(w, id.String())
+	})
+
+	http.ListenAndServe(":8081", r)
+}
